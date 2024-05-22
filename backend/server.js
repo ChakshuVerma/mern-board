3db9376e91f6b668c135cce7370df729027cb677
ChakshuVerma <vermachakshu@gmail.com>
2024-05-22 10:23:31 +0530

    Configured for production

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import path from "path";
 
 //! file imports
 import authRoutes from "./routes/auth.routes.js";
@@ -10,6 +11,8 @@ import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connect.db.js";
 import conversationRoutes from "./routes/conversation.routes.js";
 import { app, server } from "./socket/socket.js ";
+
+const __dirname = path.resolve(); // Get the current directory
 
 const PORT = process.env.PORT || 5000;
 // Increasing the http request size
@@ -28,6 +31,11 @@ app.use(cookieParser()); // To parse cookies
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/conversations", conversationRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist"))); // Serve the static files
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+}); // Send the index.html file
 
 // Start the server
 server.listen(PORT, () => {
